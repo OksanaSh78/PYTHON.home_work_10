931cab0b4595546e9b7f6cb21f48d03485b51720
user <oksi.1213@mail.ru>
2023-02-08 01:20:20 +1000

    Домашняя работа

--- venv/loader.py
+++ venv/loader.py
@@ -1,12 +1,11 @@
 from aiogram import Bot, Dispatcher
 
-# bot = Bot('')
-bot = Bot('5980825220:AAGn1eLC0R8i9WbVRi-RTqihI3t4FeuawbY')
-dp = Dispatcher(bot)
-
-
-#from aiogram import Bot, Dispatcher
-#import os
-
-#bot = Bot(os.getenv('BOT_TOKEN'))
+#bot = Bot('5980825220:AAGn1eLC0R8i9WbVRi-RTqihI3t4FeuawbY')
 #dp = Dispatcher(bot)
+
+
+from aiogram import Bot, Dispatcher
+import os
+
+bot = Bot(os.getenv('BOT_TOKEN'))
+dp = Dispatcher(bot)
